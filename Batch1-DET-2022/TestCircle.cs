21536678208964ec26a17113c35dd6fc4fbb31e5
ETG1 <ETG1@H9B1T72-SHEL>
2022-08-26 05:51:50 -0700

    add a new message console

---
+++ Batch1-DET-2022/TestCircle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022
+{
+    internal class TestCircle
+    {
+        public static void Main()
+        {
+            Circle e = new Circle(45);
+            Console.WriteLine($"The area of circle1 = {e.AreaOfCircle()}");
+        }
+    }
+}
