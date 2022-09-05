895b6d1d08350fa91e6012cfae4f1591ca7ed21c
ETG1 <ETG1@H9B1T72-SHEL>
2022-09-05 06:39:10 -0700

    add a new message

---
+++ Batch1-DET-2022/Models/program.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022.Models
+{
+    internal class Program
+    {
+     private static void Main(string[] arg)
+        {
+          //GetEmpDetailsByID();
+            Console.ReadLine();
+            // AddNewEmployee();
+            //DeleteEmployee();
+            updateEmployee();
+
+
+        }
+        private static void GetEmpDetailsByID()
+        {
+            var ctx = new trainingContext();
+            var emp = ctx.Emps.Where(e => e.Empno == 7654).SingleOrDefault();
+
+            Console.WriteLine(emp.Ename + "" + emp.Sal);
+        }
+        public static void AddNewEmployee()
+        {
+            var ctx = new trainingContext();
+
+            try
+            {
+                Emp employee = new Emp();
+                employee.Empno = 2979;
+                employee.Ename = "sheela";
+                employee.Sal = 1000;
+                employee.Deptno = 30;
+                employee.Job = "Trainner";
+                ctx.Add(employee);
+                ctx.SaveChanges();
+                Console.WriteLine("New employee added");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void DeleteEmployee()
+        {
+            var ctx = new trainingContext();
+
+            try
+            {
+                Emp employee = new Emp();
+                employee.Empno = 7369;
+                employee.Ename = "sheela";
+                employee.Sal = 1000;
+                employee.Deptno = 1;
+                employee.Job = "Trainner";
+                ctx.Remove(employee);
+                ctx.SaveChanges();
+                Console.WriteLine("New employee delete");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+
+        private static void updateEmployee()
+
+
+
+        {
+
+            var ctx = new trainingContext();
+
+            try
+
+            {
+
+                Emp employee = new Emp();
+
+                employee.Empno = 7782;
+
+                employee.Ename = "virat";
+
+                employee.Deptno = 30;
+
+                ctx.Update(employee);
+
+                ctx.SaveChanges();
+
+                Console.WriteLine("Employee updated");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                Console.WriteLine(ex.InnerException.Message);
+
+            }
+
+        }
+       
+    
+        }
+    }
---
+++ Batch1-DET-2022/Models/storedproefm.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022.Models
+{
+    internal class storedproefm
+    {
+        public static void Main()
+
+        {
+
+            Getalldetailssp();
+          //Updatenames();
+        }
+
+        private static void Getalldetailssp()
+
+        {
+
+            var ctx = new trainingContext();
+
+            // var employees = ctx.Emps.FromSqlRaw("GetAllEmpsDetails");
+
+            var employees = ctx.Emps.FromSqlRaw($"GetEmpsDetailsByEmpno @p0", 7698).ToList();
+            foreach (var e in employees)
+
+            {
+
+                Console.WriteLine(e.Ename);
+
+            }
+
+
+
+        }
+
+
+
+    }
+}
+
